1b37977ae84814c1587ce6eb06b92fbfcdaa8b84
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2023-07-19 03:13:38 +0530

    fixed editor tab close

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -131,7 +131,13 @@ const Customizer = () => {
                   <Tab 
                     key={tab.name}
                     tab={tab}
-                    handleClick={() => setActiveEditorTab(tab.name)}
+                    handleClick={() => {
+                      if(activeEditorTab===tab.name){
+                        setActiveEditorTab("")
+                      }else{
+                        setActiveEditorTab(tab.name)
+                      }
+                    }}
                   />
                 ))}
 
